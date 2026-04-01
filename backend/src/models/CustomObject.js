2e8c046412cce0083a8ec5d35e65ea53c0f38609
Jorge <jdarguedas50@gmail.com>
2026-04-01 14:40:12 -0600

    Objetos dinamicos con layout V1.0.01

--- backend/src/models/CustomObject.js
+++ backend/src/models/CustomObject.js
@@ -2,14 +2,47 @@ const mongoose = require("mongoose");
 
 const fieldSchema = new mongoose.Schema({
   label: String,
-  name: String,
+  apiName: {
+      type: String,
+      required: true,
+      default: function() {
+        return this.label.toLowerCase().replace(/\s+/g, "_");
+      },
+      unique: true
+    },
   type: String, // text, number, select
   required: Boolean,
   options: [String],
 });
+const sectionSchema = new mongoose.Schema({
+    label:{
+        type:String,
+        required: true,
+    },
+    columns:{
+        type:Number,
+        required: true,
+        default: 1,
+    },
+    fields:[String]
+});
 const layoutSchema = new mongoose.Schema({
-  section: String,
-  fields: [String], // nombres de campos
+    label:{
+        type:String,
+        required: true
+    },
+    apiName: {
+      type: String,
+      required: true,
+      default: function() {
+        return this.label.toLowerCase().replace(/\s+/g, "_");
+      },
+      unique: true
+    },
+    sections: {
+      type:[sectionSchema],
+      default:[{"label":"Detalles","columns":2,"fields":["name"]}]:
+    },
 });
 const customObjectSchema = new mongoose.Schema(
   {
@@ -20,10 +53,19 @@ const customObjectSchema = new mongoose.Schema(
     apiName: {
       type: String,
       required: true,
-      unique: true,
+      default: function() {
+        return this.name.toLowerCase().replace(/\s+/g, "_");
+      },
+      unique: true
     },
-    fields: [fieldSchema],
-    layout: [layoutSchema],
+    fields: {
+        type:[fieldSchema],
+        default:[{"label":"Name","type":"text"}]
+    }, 
+    layout: {
+      type:[layoutSchema],
+      default:[{"label":"principal"}]
+    },
   },
   { timestamps: true }
 );
