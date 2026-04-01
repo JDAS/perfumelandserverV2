edb13c100186e96adda02f3fefc5b5a9aac521d3
Jorge <jdarguedas50@gmail.com>
2026-04-01 10:57:40 -0600

    fix: api path

--- backend/src/functions/api.js
+++ backend/src/functions/api.js
@@ -1,4 +1,23 @@
 const serverless = require("serverless-http");
 const app = require("../app");
+const mongoose = require("mongoose");
 
-module.exports.handler = serverless(app);
+let isConnected = false;
+
+const connectDB = async () => {
+  if (isConnected) return;
+
+  await mongoose.connect(process.env.MONGO_URI);
+  isConnected = true;
+};
+
+const handler = serverless(app, {
+  basePath: "/.netlify/functions/api",
+});
+
+module.exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+
+  await connectDB();
+  return handler(event, context);
+};
