ff1eb3c6da71e2ae9f438cbb2a3cc3654336f453
Jorge <jdarguedas50@gmail.com>
2026-04-04 20:58:32 -0600

    triggers v1.6

--- backend/src/services/customRecordService.js
+++ backend/src/services/customRecordService.js
@@ -4,6 +4,7 @@ const { applyFormulaFields } = require("../utils/formulaEngine");
 const { recalculateParentRollupsFromChild } = require("../utils/rollupEngine");
 const { validateRecordPayload } = require("./recordValidationService");
 const { runTriggers } = require("./triggerEngine");
+const triggerEngine = require("./triggerEngine");
 console.log("TRIGGER ENGINE LOADED:", triggerEngine);
 console.log("runTriggers TYPE:", typeof triggerEngine.runTriggers);
 
